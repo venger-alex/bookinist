491d3131c63a3cb91b9651d9d541b2a13b408cc3
venger-alex <venger.alex@gmail.com>
2018-06-12 16:34:00 +0300

    refactoring, moved the creation of a connection to the class utils.DBUtils

--- src/main/java/dao/BookDAO.java
+++ src/main/java/dao/BookDAO.java
@@ -2,29 +2,20 @@ package dao;
 
 import entities.Book;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BookDAO {
-    public static List<Book> getAllBooks() {
+    public static List<Book> getAllBooks(Connection conn) {
         List<Book> resList = new ArrayList<Book>();
 
-        Connection conn = null;
         Statement stmt = null;
         ResultSet rs = null;
-        String connStr = "jdbc:postgresql://localhost:5432/bookinist";
-        String userStr = "postgres";
-        String passStr = "1";
-
         try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
-            conn = DriverManager.getConnection(connStr, userStr, passStr);
             stmt = conn.createStatement();
             rs = stmt.executeQuery("SELECT * FROM book;");
             while ( rs.next() ) {
@@ -38,7 +29,6 @@ public class BookDAO {
         } finally {
             try {rs.close();} catch (SQLException e) { }
             try {stmt.close();} catch (SQLException e) { }
-            try {conn.close(); } catch (SQLException e) { }
         }
         return resList;
     }
--- src/main/java/servlets/BookListServlet.java
+++ src/main/java/servlets/BookListServlet.java
@@ -2,6 +2,7 @@ package servlets;
 
 import entities.Book;
 import dao.BookDAO;
+import utils.DBUtils;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,18 +10,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
 import java.util.List;
 
 public class BookListServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        List<Book> bookList = BookDAO.getAllBooks();
+        Connection conn = DBUtils.getConnection();
+        List<Book> bookList = BookDAO.getAllBooks(conn);
+        DBUtils.closeConnQuietly(conn);
         req.setAttribute("bookList", bookList);
 
         RequestDispatcher requestDispatcher = req.getRequestDispatcher("views/booklist.jsp");
         requestDispatcher.forward(req, resp);
 
-        
     }
 }
